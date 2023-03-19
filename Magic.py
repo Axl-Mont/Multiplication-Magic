9a21a565dab6b765f806c93faaba500128d9218b
Axl-Mont <montenegroaxel@outlook.com>
2023-03-19 02:33:40 -0300

    se mejoro el codigo para que la lista se recorra una sola vez y el arreglo Refactorización del código para encontrar pares de factores que multiplicados dan como resultado un número ingresado. Se agregó un ciclo while en la función inicio() para permitir al usuario realizar hasta 10 intentos. También se mejoró la función busquedaDeFactores() para devolver la lista de factores encontrados, evitando el uso de una variable global. Se definieron variables localmente para mayor robustez y se eliminó código redundante. En general, se logró un código más legible y fácil de entender

--- Magic.py
+++ Magic.py
@@ -1,39 +1,47 @@
-factores=[]
 
 def inicio():
-        print('¿Le gustaría saber qué pares de números enteros, cuando son multiplicados entre sí, dan como resultado un número que usted elija?\n')
-        number=int(input('Si es así, por favor ingrese el número que desea investigar: '))
+    print('¿Le gustaría saber qué pares de números enteros, cuando son multiplicados entre sí, dan como resultado un número que usted elija?')
+    intentos = 0
+    while intentos < 10:
+        number = int(input('\nIngrese el número que desea investigar: '))
         if esPrimo(number):
-                print (f'\nEl numero {number} es un numero primo, no posee esta caracteristica')
+            print( f'\nEl numero {number} es un numero primo, no posee esta caracteristica')
         elif number != 0 and number > 1:
-                busquedaDeFactores(number)
-                busquedaDeMultiplicacion (number)
+            factores = busquedaDeFactores(number)
+            busquedaDeMultiplicacion(number, factores)
         else:
-                print('\n0 y 1 no son elegibles, intenta con otro numero')
+            print('\n0 y 1 no son elegibles, intenta con otro numero')
+        intentos +=1
+        print(f'\nIntentos {intentos} de 10')
+
 
 def busquedaDeFactores(n):
-        cont = 1
-        while cont != n:
-                if n % cont == 0:
-                        factores.append(cont)
-                cont +=1
+    cont = 1
+    factores = []
+    while cont != n:
+        if n % cont == 0:
+            factores.append(cont)
+        cont += 1
+    return factores
 
-def busquedaDeMultiplicacion(n):
+
+def busquedaDeMultiplicacion(n, f):
     print('\n Resultado: \n')
-    for i in range(len(factores)):
-        for j in range(len(factores)):
-            if factores[i] * factores[j] == n:
-                print(f'{factores[i]} * {factores[j]} = {n}')
+    for i in range(len(f)):
+        for j in range(i, len(f)):
+            if f[i] * f[j] == n:
+                print(f'{f[i]} * {f[j]} = {n}')
+
 
 def esPrimo(n):
-        if n == 2:
-                return True
-        elif n % 2 == 0:
-                return False
-        for i in range(3, int(n**0.5)+1, 2):
-                if n % i == 0:
-                        return False
+    if n == 2:
         return True
+    elif n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5)+1, 2):
+        if n % i == 0:
+            return False
+    return True
 
 
 inicio()
