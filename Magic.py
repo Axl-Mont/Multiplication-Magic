6a5397c7d75eb44882cf9ca74ca46514b44816c9
Axl-Mont <montenegroaxel@outlook.com>
2023-03-19 00:48:51 -0300

    se agrego condicion para que 0 y 1 no sean elegibles

--- Magic.py
+++ Magic.py
@@ -1,9 +1,13 @@
+factores=[]
 
 def inicio():
         print('¿Le gustaría saber qué pares de números enteros, cuando son multiplicados entre sí, dan como resultado un número que usted elija?')
         number=int(input('Si es así, por favor ingrese el número que desea investigar: '))
-        busquedaDeFactores(number)
-        busquedaMultiplicacion (number)
+        if number != 0 and number > 1:
+                busquedaDeFactores(number)
+                busquedaDeMultiplicacion (number)
+        else:
+                print('0 y 1 no son elegibles, intenta con otro numero')
 
 def busquedaDeFactores(n):
         cont = 1
@@ -12,12 +16,10 @@ def busquedaDeFactores(n):
                         factores.append(cont)
                 cont +=1
 
-def busquedaMultiplicacion(n):
+def busquedaDeMultiplicacion(n):
     for i in range(len(factores)):
         for j in range(len(factores)):
             if factores[i] * factores[j] == n:
                 print(f'{factores[i]} * {factores[j]} = {n}')
-
-factores=[]
 
 inicio()
