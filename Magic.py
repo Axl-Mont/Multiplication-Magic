e172e1da4c60f7e8b3e12253d9ad0b896ecf33b3
Axl-Mont <montenegroaxel@outlook.com>
2023-03-19 01:18:12 -0300

    se agrego la verificacion si el numero ingresado en primo

--- Magic.py
+++ Magic.py
@@ -3,7 +3,9 @@ factores=[]
 def inicio():
         print('¿Le gustaría saber qué pares de números enteros, cuando son multiplicados entre sí, dan como resultado un número que usted elija?')
         number=int(input('Si es así, por favor ingrese el número que desea investigar: '))
-        if number != 0 and number > 1:
+        if esPrimo(number):
+                print (f'El numero {number} es un numero primo, no posee esta caracteristica')
+        elif number != 0 and number > 1:
                 busquedaDeFactores(number)
                 busquedaDeMultiplicacion (number)
         else:
@@ -22,4 +24,16 @@ def busquedaDeMultiplicacion(n):
             if factores[i] * factores[j] == n:
                 print(f'{factores[i]} * {factores[j]} = {n}')
 
+def esPrimo(n):
+        if n == 2:
+                return True
+        elif n % 2 == 0:
+                return False
+        for i in range(3, int(n**0.5)+1, 2):
+                if n % i == 0:
+                        return False
+        return True
+
+
 inicio()
+input()
